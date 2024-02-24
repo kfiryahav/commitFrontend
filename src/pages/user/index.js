0002ca3c85eff350427772e6f270b7bbb2e05406
kfirY <81631340+kfiryahav@users.noreply.github.com>
2024-02-24 21:02:21 +0200

    Update index.js
    removed redundant code

--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -2,28 +2,9 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import RootLayout from "@/app/layout";
 import UserDetails from "../../../components/user/userDetailsComponent";
-import { getUser } from '@/services/apiService';
-import { setUserData } from '@/redux/actions';
-import cookie from 'js-cookie'; // Import the js-cookie library
 
 function User() {
-    const dispatch = useDispatch();
     const userData = useSelector(state => state.userData);
-
-    useEffect(() => {
-        const email = cookie.get('email');
-        if (email && userData.email !== email) {
-            getUser(email)
-                .then(userData => {
-                    dispatch(setUserData(userData));
-                })
-                .catch(error => {
-                    console.error('Error fetching user data:', error);
-                    dispatch(setUserData(null));
-                });
-        }
-    }, [dispatch]);
-
     return (
         <>
             <RootLayout>
